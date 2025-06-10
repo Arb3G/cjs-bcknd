9d81167994c5bead806766255eb3bba6f7bb6613
Arb3G <rbg242570@gmail.com>
2025-06-10 14:14:29 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,8 @@
+async function sendCJS(wallet, amount) {
+  // Here you'd use Stellar SDK to send payment
+  console.log(`🔁 [Mock] Sending ${amount} CJS to wallet: ${wallet}`);
+}
 
+module.exports = {
+  sendCJS
+};
